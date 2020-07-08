c93bfb397edb8525cd35355cf65589f00f8bd97c
tywi6665 <tyletcole@gmail.com>
2020-07-08 13:20:37 -0600

    Creating overlay logic

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,18 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SubContainer from '../Components/Containers/Subcontainer';
 import ImageWrapper from "../Components/ImageWrapper";
 
 const Main = ({ setCurrentPage }) => {
+
+    const [hover, setHover] = useState("")
+    const overlays = {
+        inflow: ["inflow-1", "inflow-2"]
+    }
+
     return (
         <SubContainer>
-            <ImageWrapper
-                src={"mainmenu"}
-            />
+            <div className="img-overlay-container">
+                <ImageWrapper
+                    src={"mainmenu"}
+                />
+                <div className="overlay">
+                    {hover ? (
+                        hover.map(overlay => (
+                            <div className={`${overlay} circle`}></div>
+                        ))
+                    ) : (null)}
+                </div>
+            </div>
             <ul className="menu-list">
-                <li className="clickable" onClick={e => setCurrentPage("Inflow")}>Inflow</li>
-                <li className="clickable" onClick={e => setCurrentPage("Rainbands")}>Rainbands</li>
-                <li className="clickable" onClick={e => setCurrentPage("Eye&eyewall")}>Eye & Eyewall</li>
-                <li className="clickable" onClick={e => setCurrentPage("Outflow")}>Outflow</li>
+                <li className="clickable"
+                    onClick={e => setCurrentPage("Inflow")}
+                    onMouseEnter={() => setHover(overlays.inflow)}
+                    onMouseLeave={() => setHover("")}
+                >Inflow</li>
+                <li className="clickable"
+                    onClick={e => setCurrentPage("Rainbands")}
+                    onMouseEnter={() => setHover("rainbands")}
+                    onMouseLeave={() => setHover("")}
+                >Rainbands</li>
+                <li className="clickable"
+                    onClick={e => setCurrentPage("Eye&eyewall")}
+                    onMouseEnter={() => setHover("eyewall")}
+                    onMouseLeave={() => setHover("")}
+                >Eye & Eyewall</li>
+                <li className="clickable"
+                    onClick={e => setCurrentPage("Outflow")}
+                    onMouseEnter={() => setHover("outflow")}
+                    onMouseLeave={() => setHover("")}
+                >Outflow</li>
             </ul>
         </SubContainer>
     );
